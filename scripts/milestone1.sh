515c102e92b56cf01606ecbc9fe8105689829c68
dmholtz <56723830+dmholtz@users.noreply.github.com>
2021-12-19 10:26:04 +0100

    Add variable-writes analysis for milestone 1

---
+++ scripts/milestone1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+while getopts s:a: flag; do
+    case "${flag}" in
+    a) analysis=${OPTARG} ;;
+    s) script=${OPTARG} ;;
+    esac
+done
+
+if [ -z "$analysis" ]; then
+    analysis="../scripts/variable_writes.js"
+fi
+
+if [ -z "$script" ]; then
+    script="../testcases/milestone1/test1.js"
+fi
+
+echo "script: $script"
+echo "analysis: $analysis"
+
+#node ../../jalangi2/src/js/commands/jalangi.js --inlineIID --inlineSource --analysis $analysis $script
+node ../../jalangi2/src/js/commands/jalangi.js --analysis ../../jalangi2/src/js/runtime/SMemory.js --inlineSource --analysis $analysis $script
---
+++ scripts/variable_writes.js
@@ -0,0 +1,31 @@
+
+(function () {
+    let writes = [];
+
+    J$.analysis = {
+
+        write: function (iid, name, val, oldValue) {
+            var id = J$.getGlobalIID(iid);
+            writes.push({ name: name, value: val });
+        },
+
+        /**
+         * This callback is called when an execution terminates in node.js.  In a browser
+         * environment, the callback is called if ChainedAnalyses.js or ChainedAnalysesNoCheck.js
+         * is used and Alt-Shift-T is pressed.
+         *
+         * @returns {undefined} - Any return value is ignored
+         */
+        endExecution: function () {
+            for (let id in writes) {
+                var writeInfo = writes[id];
+                var location = J$.iidToLocation(id);
+                console.log("Write value " + writeInfo.value +
+                    " to variable " + writeInfo.name + ".");
+            }
+            console.log(J$.smemory)
+        }
+    };
+
+}());
+
