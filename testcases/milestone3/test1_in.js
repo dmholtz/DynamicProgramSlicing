a0263d9fc991849d23a97dfea0e4a5835567dec6
dmholtz <56723830+dmholtz@users.noreply.github.com>
2021-12-31 12:15:24 +0100

    Add control-flow testcase

---
+++ testcases/milestone3/test1_in.js
@@ -0,0 +1,12 @@
+function sliceMe() {
+    var x = 0;
+    var y = 1;
+    if (y) {
+        y++;
+    }
+    else {
+        y--;
+    }
+    return x;
+}
+sliceMe();
---
+++ testcases/milestone3/test1_out.js
@@ -0,0 +1,5 @@
+function sliceMe() {
+    var x = 0;
+    return x;
+}
+sliceMe();
